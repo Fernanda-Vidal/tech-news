364847bf97cc6967915da46e5819af1358c13730
Fernanda Vidal <fevidal.dev@gmail.com>
2023-02-15 08:25:34 -0300

    feat: Busca notícia por data

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,20 +1,35 @@
 from tech_news.database import search_news
+from datetime import datetime
+
 
 # Requisito 7
 def search_by_title(title):
-    query = search_news({"title": {"$regex": title, "$options": "i"}})
+    search = search_news({"title": {"$regex": title, "$options": "i"}})
     list_title = []
 
-    for index in query:
+    for index in search:
         new = (index["title"], index["url"])
         list_title.append(new)
-    
+
     return list_title
 
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    list_search = []
+    
+    try: 
+        date_fomatted = datetime.strptime(date, "%Y-%m-%d").strftime("%d/%m/%Y")
+        search = search_news({"timestamp": {"$regex": date_fomatted}})
+        for index in search:
+            new = (index["title"], index["url"])
+            list_search.append(new)
+    except ValueError:
+        raise ValueError("Data inválida")
+    
+    else:
+        return list_search
+
 
 
 # Requisito 9
